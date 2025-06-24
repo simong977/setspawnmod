9aeb5a3385d5b05bb3a0c9d22eef87a187ec9e7f
simong977 <ehddnzz@gmail.com>
2025-06-25 02:54:07 +0900

    Create build.yml

--- gradlew.bat
+++ gradlew.bat
@@ -1,2 +1,2 @@
 @echo off
-java -jar gradle\wrapper\gradle-wrapper.jar
+java -jar gradle\wrapper\gradle-wrapper.jar %*
---
+++ src/main/java/com/simong/setspawnmod/SetSpawnMod.java
@@ -0,0 +1,13 @@
+
+package com.simong.setspawnmod;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.fml.common.Mod;
+
+@Mod("setspawnmod")
+public class SetSpawnMod {
+    public SetSpawnMod() {
+        System.out.println("SetSpawnMod loaded.");
+    }
+}
